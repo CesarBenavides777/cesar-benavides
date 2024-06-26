2ddb49157f9d35cbfa52860bef6aa021e68056ac
Cesar Benavides <cesarbenavides@icloud.com>
2024-06-26 12:59:32 -0500

    Adding custom login handler

--- faust.config.js
+++ faust.config.js
@@ -1,7 +1,9 @@
-import { setConfig } from '@faustwp/core/dist/mjs/config/index.js';
-import possibleTypes from './possibleTypes.json';
+import { setConfig } from "@faustwp/core/dist/mjs/config/index.js";
+import possibleTypes from "./possibleTypes.json";
 
 /** @type {import('@faustwp/core').FaustConfig} */
 export default setConfig({
+  wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
+  apiClientSecret: process.env.FAUST_SECRET_KEY,
   possibleTypes,
 });
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ import { createSecureHeaders } from 'next-secure-headers';
 
 /** @type {import('next').NextConfig} */
 export default withFaust({
+  trailingSlash: false,
   async headers() {
     return [{ source: '/:path*', headers: createSecureHeaders({
       xssProtection: false
