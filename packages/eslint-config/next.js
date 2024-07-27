9c95b52285adfadf2a0c631f375eb35dfd415449
Cesar Benavides <cesarbenavides@icloud.com>
2024-07-27 11:42:30 -0500

    cjs

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -5,7 +5,7 @@ const project = resolve(process.cwd(), "tsconfig.json");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
-    "eslint-plugin-react-compiler",
+    "eslint:recommended",
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
     "turbo",
@@ -32,7 +32,4 @@ module.exports = {
     "node_modules/",
   ],
   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-  rules: {
-    "react-compiler/react-compiler": "error",
-  },
 };
