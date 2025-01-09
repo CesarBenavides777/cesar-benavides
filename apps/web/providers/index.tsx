26429d2af01790c1863cbb9dd4c7412f3ffe147e
Cesar Benavides <cesarbenavides@icloud.com>
2025-01-08 19:35:11 -0600

    add PostHog integration for user analytics

--- apps/web/providers/index.tsx
+++ apps/web/providers/index.tsx
@@ -2,11 +2,26 @@
 
 import ThemeProvider from "./ThemeProvider";
 import { FaustProvider } from "@faustwp/experimental-app-router/ssr";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+  });
+}
+
 
 const Providers = ({ children }) => {
   return (
     <FaustProvider>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <PostHogProvider client={posthog}>
+          {children}
+        </PostHogProvider>
+      </ThemeProvider>
     </FaustProvider>
   );
 };
