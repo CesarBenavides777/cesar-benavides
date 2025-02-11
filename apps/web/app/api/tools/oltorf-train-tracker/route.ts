2ddb422b8a7c0adc9db81bb44a94ca23e7f7f71f
Cesar Benavides <cesarbenavides@icloud.com>
2025-02-11 11:09:17 -0600

    fixes

--- apps/web/app/api/tools/oltorf-train-tracker/route.ts
+++ apps/web/app/api/tools/oltorf-train-tracker/route.ts
@@ -9,6 +9,9 @@ const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
 const client: Twilio = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 export async function GET(request: Request) {
+  if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  }
     const response = await fetch(ENDPOINT)
     const data = await response.json()
 
