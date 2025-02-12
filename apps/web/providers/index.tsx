e02c3c0e3014ea3af327bb678fd30193de38bcf5
Cesar Benavides <cesarbenavides@icloud.com>
2025-02-12 15:01:57 -0600

    fixing css styles

--- apps/web/providers/index.tsx
+++ apps/web/providers/index.tsx
@@ -1,30 +1,13 @@
 "use client";
 
 import ThemeProvider from "./ThemeProvider";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
 import { ApolloProvider } from "./apollo/ssr";
-import { useEffect } from "react";
-
-const isDev = process.env.NODE_ENV === "development";
 
 const Providers = ({ children }) => {
 
-    useEffect(() => {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      });
-    }, []);
-
   return (
     <ApolloProvider>
-      <ThemeProvider>
-        {isDev ? (
-          children
-        ) : (
-          <PostHogProvider client={posthog}>{children}</PostHogProvider>
-        )}
-      </ThemeProvider>
+      <ThemeProvider>{children}</ThemeProvider>
     </ApolloProvider>
   );
 };
